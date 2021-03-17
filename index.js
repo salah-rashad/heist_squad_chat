e5680ca871f1e00c9d991998d4cc62cd1f9bb60e
salah-rashad <sla7.r4ad@gmail.com>
2021-03-17 08:54:45 +0200

    fixing some issues

--- index.js
+++ index.js
@@ -8,47 +8,45 @@ port = process.env.PORT || 3000;
 //   res.send("Node Server is running. Yay!!")
 // })
 
-// io.on('connection', function (socket) {
-//   //Get the roomID of the user and join in a room of the same roomID
-//   roomID = socket.handshake.query.roomID
-//   socket.join(roomID)
 
-//   //Leave the room if the user closes the socket
-//   socket.on('disconnect', () => {
-//     socket.leave(roomID)
-//   })
+io.on('connection', function (socket) {
 
-//   //Send message to only a particular user
-//   socket.on('send_message', message => {
-//     receiverRoomID = message.receiverRoomID
-//     senderRoomID = message.senderRoomID
-//     content = message.content
+  //Get the roomID of the user and join in a room of the same roomID
+  roomID = socket.handshake.query.roomID
+  socket.join(roomID);
 
-//     //Send message to only that particular room
-//     socket.in(receiverRoomID).emit('receive_message', {
-//       'content': content,
-//       'senderRoomID': senderRoomID,
-//       'receiverRoomID': receiverRoomID,
-//     })
-//   })
-// });
-
-
-
-io.on("connection", function (client) {
-  console.log("Connected Client:: ", client);
-
-  client.on("joinRoom", (roomId, username) => {
-    client.join(roomId);
-    io.to(roomId).emit("sendMessage", username + " joined room " + roomId);
+  client.on("JOIN_ROOM", (room, nickname) => {
+    socket.in(room).emit("SEND_MESSAGE", nickname + " joined room.");
   });
-  client.on(
-    "sendMessage",
-    (message, roomId, username) => {
-      io.to(roomId).emit("sendMessage", message, username);
-    }
-  );
+
+  //Leave the room if the user closes the socket
+  socket.on('disconnect', () => {
+    socket.leave(roomID)
+  });
+
+  // Receive a message from client in a particular room
+  socket.on('SEND_MESSAGE', (room, nickname, content) => {
+    //Send message to only that particular room
+    socket.in(room).emit('RECEIVE_MESSAGE', (room, nickname, content));
+  })
 });
+
+
+
+// io.on("connection", function (client) {
+//   console.log("Connected Client:: ", client);
+
+//   client.on("joinRoom", (roomId, username) => {
+//     client.join(roomId);
+//     io.to(roomId).emit("sendMessage", username + " joined room " + roomId);
+//   });
+//   client.on(
+//     "sendMessage",
+//     (message, roomId, username) => {
+//       io.to(roomId).emit("sendMessage", message, username);
+//     }
+//   );
+// });
 
 
 http.listen(port, function () {
